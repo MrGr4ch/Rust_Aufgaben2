819d562fb01f152da3c87bd67fca7bc6d34cf2bf
MrGr4ch <104979051+MrGr4ch@users.noreply.github.com>
2023-05-22 17:24:00 +0200

    Update url.rs

--- src/lexer/url.rs
+++ src/lexer/url.rs
@@ -1,4 +1,4 @@
-use logos::{Lexer, Logos, Source};
+use logos::{Lexer, Logos};
 use std::fmt::{Display, Formatter};
 
 /// Tuple struct for link URLs
@@ -25,7 +25,7 @@ impl Display for LinkText {
 
 #[derive(Logos, Debug, PartialEq)]
 pub enum URLToken {
-    #[regex(r#"<a[ \n]*[^>]*href\s*=\s*"([^"]*)"[^>]*>([^<]*)</a>"#, extract_link_info)]
+    #[regex(r#"<a[ \n\t\f\r]*[^>]*href\s*=\s*"([^"]*)"[^>]*>([^<]*)</a[ \n\t\f\r]*>"#, extract_link_info)]
     Link((LinkUrl, LinkText)),
 
     #[regex(r"[ \t\n\f\r]+", logos::skip)] // Ignore whitespaces
@@ -38,12 +38,16 @@ pub enum URLToken {
 
 /// Extracts the URL and text from a string that matched a Link token
 fn extract_link_info(lex: &mut Lexer<URLToken>) -> (LinkUrl, LinkText) {
+    println!("gefunden!");
     let token_slice = lex.slice();
-
+    println!("{:?}",token_slice);
     let last_quote = token_slice.rfind('"').unwrap();
+    println!("{:?}",last_quote);
     let (url, text) = token_slice.split_at(last_quote + 1);  // +1 to exclude the quote itself from the URL
     let url = url.trim_end_matches('"'); // remove ending quote from URL
+    println!("{:?}",url);
     let text = text.trim_start_matches(' '); // remove leading whitespace from text
+    println!("{:?}",text);
 
-    (LinkUrl(url.into()), LinkText(text.into()))
+    (LinkUrl(url.parse().unwrap()), LinkText(text.parse().unwrap()))
 }
